d27e3577be52168f161221e0016928f42a59640c
ceptiloid <nikolaidichenko@gmail.com>
2024-12-06 02:13:38 +0300

    user файл

---
+++ user.py
@@ -0,0 +1,7 @@
+# Запрашиваем ввод данных от пользователя
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+# Выводим информацию об пользователе
+print(f"Hello, {name}! You are {age} years old and you live in {location}.")
